5593a2efdf8ac0c3a366c3436dc18827329ca782
ken-obieze <kenneth.obieze@outlook.com>
2025-11-29 16:02:28 +0100

    feature: add Organization model with slug generation and admin integration

--- pollr_backend/pollr_backend/settings.py
+++ pollr_backend/pollr_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'organizations.apps.OrganizationsConfig',
+    'pollr.apps.PollrConfig',
 ]
 
 MIDDLEWARE = [
